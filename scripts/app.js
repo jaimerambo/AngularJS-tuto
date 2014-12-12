a7ad1a7deefd98c81b15c5873e0384b9e4d69d55
jaime h <jaimeestudiante1@gmail.com>
2014-12-12 11:23:12 -0500

    ñ

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+
+
+function AlumnosController($scope){
+    $scope.alumnos=[
+        {nombre:"JAime",telefono:"das" ,curso:"9"},
+        {nombre:"JAime",telefono:"das" ,curso:"9"},
+        {nombre:"JAime",telefono:"das" ,curso:"9"}
+    ]
+
+}
