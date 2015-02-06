c5fdf0e7ecfc3581052a081ae7c2a3cc3433770f
jaime h <jaimeestudiante1@gmail.com>
2015-02-06 13:39:22 -0500

    add new functions for agg name

--- scripts/app.js
+++ scripts/app.js
@@ -16,4 +16,35 @@
 
 
     })
+
+    app.controller("peticionesGET",['$scope','$http','$templateCache', function ($scope, $http, $templateCache) {
+        $scope.method='GET';
+        $scope.url='index.html'
+
+        $scope.fetch= function () {
+            $scope.code=null;
+            $scope.response=null;
+            $http({method:$scope.method,url:$scope.url,cache:$templateCache})
+                .success(function (data,status) {
+                    $scope.status=status;
+                    $scope.data=data;
+                })
+                .error(function (data, status) {
+                    $scope.status=status;
+                    $scope.data=data || "Request failed";
+                });
+
+            $scope.updateModel = function(method, url) {
+                $scope.method = method;
+                $scope.url = url;
+            };
+        }
+
+    }]);
+
+
+
+
+
+
 })();
